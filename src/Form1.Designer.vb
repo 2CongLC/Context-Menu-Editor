4520f025d80886573faab3c42583c9efa603588a
ChuCong <2conglc.vn@gmail.com>
2024-05-31 09:00:53 +0700

    update

---
+++ src/Form1.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.OpenFileDialog2 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(3, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(478, 163)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Thiết lập ( Option )"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(379, 53)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(90, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Chọn ..."
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(379, 22)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(90, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Chọn ..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Bên trên (Top)", "Ở giữa (Mid)", "Bên dưới (Bottom)"})
+        Me.ComboBox1.Location = New System.Drawing.Point(121, 112)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(244, 21)
+        Me.ComboBox1.TabIndex = 7
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(121, 84)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(244, 20)
+        Me.TextBox3.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(121, 55)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(244, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(121, 24)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(244, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(28, 115)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(84, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Vị trí (Position) : "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(18, 84)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(97, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Tên trong (menu) : "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 55)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Biểu tượng (Icon) : "
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(94, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Ứng dụng (Path) : "
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(50, 181)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(185, 36)
+        Me.Button3.TabIndex = 1
+        Me.Button3.Text = "Thêm"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(253, 181)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(185, 36)
+        Me.Button4.TabIndex = 2
+        Me.Button4.Text = "Xóa"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(412, 239)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(60, 13)
+        Me.LinkLabel1.TabIndex = 3
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "HomePage"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 261)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ContextMenu Editor - Chỉnh sửa menu chuột phải"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents OpenFileDialog2 As OpenFileDialog
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents LinkLabel1 As LinkLabel
+End Class
---
+++ src/Form1.vb
@@ -0,0 +1,73 @@
+﻿Imports System
+Imports System.Text
+Imports System.IO
+Imports Microsoft.Win32
+
+Public Class Form1
+    Private Reg As RegistryKey
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.SelectedIndex = 1
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            TextBox1.Text = OpenFileDialog1.FileName.Trim
+            TextBox3.Text = Path.GetFileName(TextBox1.Text)
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If OpenFileDialog2.ShowDialog = DialogResult.OK Then
+            TextBox2.Text = OpenFileDialog2.FileName.Trim
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            If File.Exists(TextBox1.Text) = True OrElse File.Exists(TextBox2.Text) = True Then
+                If String.IsNullOrWhiteSpace(TextBox3.Text) = False Then
+
+                    Registry.ClassesRoot.OpenSubKey("Directory\Background\shell\" + TextBox3.Text, True)
+                    If Reg Is Nothing Then My.Computer.Registry.ClassesRoot.CreateSubKey("Directory\Background\shell\" + TextBox3.Text, True)
+                    My.Computer.Registry.ClassesRoot.OpenSubKey("Directory\Background\shell\" + TextBox3.Text, True).SetValue("Icon", Chr(34) & TextBox1.Text & Chr(34), RegistryValueKind.String)
+                    Dim p As String = String.Empty
+                    If ComboBox1.SelectedIndex = 0 Then
+                        p = "top"
+                    ElseIf ComboBox1.SelectedIndex = 1 Then
+                        p = "midle"
+                    ElseIf ComboBox1.SelectedIndex = 2 Then
+                        p = "bottom"
+                    End If
+                    My.Computer.Registry.ClassesRoot.OpenSubKey("Directory\Background\shell\" + TextBox3.Text, True).SetValue("Position", p, RegistryValueKind.String)
+                    My.Computer.Registry.ClassesRoot.CreateSubKey("Directory\Background\shell\" + TextBox3.Text + "\" + "Command\").SetValue("", Chr(34) & TextBox2.Text & Chr(34), RegistryValueKind.String)
+
+                    MessageBox.Show("Đã thêm vào menu !!!")
+                Else
+                    MessageBox.Show("Nhập tên ứng dụng trong menu !")
+                End If
+            Else
+                MessageBox.Show("Hãy chọn đúng đường dẫn ứng dụng và biểu tượng !")
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Try
+            My.Computer.Registry.ClassesRoot.DeleteSubKeyTree("Directory\Background\shell\" + TextBox3.Text, True)
+            MessageBox.Show("Đã xóa khỏi menu !!!")
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Try
+            Process.Start("https://2conglc-vn.blogspot.com/2020/08/cong-cu-chinh-sua-menu-chuot-phai.html")
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
